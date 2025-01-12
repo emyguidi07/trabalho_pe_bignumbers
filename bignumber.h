02855a942ccb93ed4b164c7dc98399d7b859e4d5
palomaval <72564003+palomaval@users.noreply.github.com>
2025-01-12 17:36:13 -0300

    Update bignumber.h

--- bignumber.h
+++ bignumber.h
@@ -35,4 +35,5 @@ BigNumber *stringToBigNumber(const char *str);
 BigNumber *exponentiate(BigNumber *base, BigNumber *exponent); 
 BigNumber *modulo(BigNumber *a, BigNumber *b);
 BigNumber *restoDivisao(BigNumber *a, BigNumber *b);
+BigNumber *divisaoInteira(BigNumber *a, BigNumber *b)
 #endif
